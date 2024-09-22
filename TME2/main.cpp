a501e611e7f3f24c6e2a723095a82571fbe51dc7
CH-RMyV <181732172+CH-RMyV@users.noreply.github.com>
2024-09-22 19:06:16 +0200

    Ajout comptage des mots différents et leurs occurences (pas encore d'affichage)

---
+++ TME2/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <chrono>
+#include <vector>
+#include <string>
+
+
+int isPresent(const std::vector<std::pair<std::string, uint32_t>> &vec, const std::string &str)
+{
+	int i;
+	for(i = 0; i<vec.size(); ++i)
+	{
+		if(str == std::get<0>(vec[i])) return i;
+	}
+	return -1;
+}
+
+int main () {
+	using namespace std;
+	using namespace std::chrono;
+
+	ifstream input = ifstream("WarAndPeace.txt");
+	int rank;
+	vector<pair<string, uint32_t>> mots;
+
+	auto start = steady_clock::now();
+	cout << "Parsing War and Peace" << endl;
+
+	size_t nombre_lu = 0;
+	// prochain mot lu
+	string word;
+	// une regex qui reconnait les caractères anormaux (négation des lettres)
+	regex re( R"([^a-zA-Z])");
+	while (input >> word) {
+		// élimine la ponctuation et les caractères spéciaux
+		word = regex_replace ( word, re, "");
+		// passe en lowercase
+		transform(word.begin(),word.end(),word.begin(),::tolower);
+
+		rank = isPresent(mots, word);
+		if(rank==-1)
+		{
+			pair<string, uint32_t> newWord = make_pair(word, 1);
+			mots.push_back(newWord);
+		}
+		else
+		{
+			mots[rank] = make_pair(word, get<1>(mots[rank])+1);
+		}
+
+		// word est maintenant "tout propre"
+		if (nombre_lu % 100 == 0)
+			// on affiche un mot "propre" sur 100
+			cout << nombre_lu << ": "<< word << endl;
+		nombre_lu++;
+	}
+	input.close();
+
+	cout << "Finished Parsing War and Peace" << endl;
+
+	auto end = steady_clock::now();
+    cout << "Parsing took "
+              << duration_cast<milliseconds>(end - start).count()
+              << "ms.\n";
+
+    cout << "Found a total of " << nombre_lu << " words." << endl;
+
+	cout << "found a total of " << mots.size() << " different words." << endl;
+
+    return 0;
+}
+
+
