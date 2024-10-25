344a5c64c7058cc84d25ead01522aa5524571789
CH-RMyV <181732172+CH-RMyV@users.noreply.github.com>
2024-10-25 15:46:06 +0200

    Ex1.2 fait

--- TME6/src/fork_exo.cpp
+++ TME6/src/fork_exo.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main () {
 	const int N = 3;
-	std::cout << "main pid=" << getpid() << std::endl;
+	int children = 0;
+	std::cerr << "main pid=" << getpid() << std::endl;
 
-	for (int i=1, j=N; i<=N && j==N && fork()==0 ; i++ ) {
-		std::cout << " i:j " << i << ":" << j << std::endl;
-		for (int k=1; k<=i && j==N ; k++) {
-			if ( fork() == 0) {
-				j=0;
-				std::cout << " k:j " << k << ":" << j << std::endl;
+	for (int i=1, j=N; i<=N && j==N ; i++)
+	{
+		if(fork()==0)
+		{
+			children = 0;
+			std::cerr << " i:j " << i << ":" << j << std::endl;
+			for (int k=1; k<=i && j==N ; k++) {
+				if ( fork() == 0) {
+					children = 0;
+					j=0;
+					std::cerr << " k:j " << k << ":" << j << std::endl;
+				}
+				else
+				{
+					children++;
+				}
 			}
 		}
+		else
+		{
+			children++;
+			break;
+		}
 	}
+
+	//std::cout << "children : " << children << " pid : " << getpid() << std::endl;
+	for(int i = 0; i<children; ++i)
+	{
+		wait(NULL);
+	}
+	std::cerr << getpid() << std::endl;
 	return 0;
 }
