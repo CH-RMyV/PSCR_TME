0fc7e88dabe20c93a13fd2add655b42d1e54656d
CH-RMyV <181732172+CH-RMyV@users.noreply.github.com>
2024-11-15 19:46:39 +0100

    Commandes décomposées

--- TME7/src/Ex1.cpp
+++ TME7/src/Ex1.cpp
@@ -8,9 +8,6 @@ int main(int argc, char** argv)
     char* cmd[argc+1];
     memcpy(cmd, argv, sizeof(char*)*(argc+1));
 
-    //std::cout << cmd[1] << std::endl;
-    //std::cout << cmd[2] << std::endl;
-
     int pfd[2];
     if(pipe(pfd)!=0)
     {
@@ -22,12 +19,9 @@ int main(int argc, char** argv)
 
     for(i = 1; i<argc; ++i)
     {
-        if(strcmp(argv[i], "|"))
+        if(strcmp(argv[i], "|") == 0)
             break;
     }
-
-    //cmd 1 va de argv[1] à argv[i-1]
-    //cmd 2 va de argv[i+1] à argv[argc-1]
 
     if(i == argc)
     {
@@ -35,12 +29,22 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    cmd[i] = NULL;
+    cmd[i] = NULL;  //On remplace le | par 0x0 pour pouvoir donner directement les arguments sans autre traitement lors d'exec
 
-    char* cmd1[i+2];
-    char* cmd2[argc-i+2];
+    char* cmd1[i];
+    char* cmd2[argc-i];
 
-    memcpy(cmd1, argv[1], sizeof(char*)*(i+2));
-    memcpy(cmd2, argv[i+1], sizeof(char*)*(argc-i+2));
+    memcpy(cmd1, cmd+1, sizeof(cmd1));
+    memcpy(cmd2, cmd+i+1, sizeof(cmd2));
+
+    if(fork() == 0)   //Première commande
+    {
+        execv(cmd1[0], cmd1);
+    }
+
+    if(fork() == 0) //Deuxième commande
+    {
+        execv(cmd2[0], cmd2);
+    }
 
 }
